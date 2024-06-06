f2fcc476d1721682e9417f1d92cc0b52c1184470
古堅　まり子 <159230391+mrkfrgn@users.noreply.github.com>
2024-06-06 11:55:22 +0900

    Add files via upload

--- kadai_sort/sort.php
+++ kadai_sort/sort.php
@@ -7,23 +7,34 @@
 </head>
 
 <body>
-  <p>
-    <?php
-    
-    echo "昇順にソートします。". "<br>";
-    $nums = [15, 4, 18, 23, 10];
-    sort($nums);
-    foreach ($nums as $key => $val){
-      echo $val. "<br>";
-    }
-    echo "降順にソートします。". "<br>";
-    $nums = [15, 4, 18, 23, 10];
-    rsort($nums);
-    foreach ($nums as $key => $val){
-      echo $val. "<br>";
-    }
-    ?>
-    </p>
+    <p>
+      <?php
+      
+      $nums = [15, 4, 18, 23, 10];
 
+function sort_2way($array, $order) {
+    if ($order === 'asc') {
+        sort($array); 
+    } elseif ($order === 'desc') {
+        rsort($array); 
+    } 
+    return $array;
+}
+
+$sorted = sort_2way($nums, 'asc');
+echo "昇順にソートします。<br> " ;
+foreach ($sorted as $value){
+  echo $value . "<br>";
+}
+
+
+$r_sorted = sort_2way($nums, 'desc');
+echo "降順にソートします。<br>  ";
+foreach ($r_sorted as $value){
+  echo $value . "<br>";
+}
+
+      ?>
+  </p>
 </body>
 </html>
